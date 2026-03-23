19cfd6c5eae8b129d89cb56cfd595d672391bde0
Akil0814 <akil040814@gmail.com>
2026-03-23 16:07:00 -0700

    添加项目文件。

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_CS_1D_BaseBall_FILE C:/CS1D_P1/CS_1D_BaseBall/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/CS_1D_BaseBall.exe)
+set(__QT_DEPLOY_TARGET_CS_1D_BaseBall_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_CS_1D_BaseBall_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
