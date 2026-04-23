296a1879521759a72f659dd46b45702da3042648
Erfan Tavassoli <106490629+Erfan-TAV@users.noreply.github.com>
2026-04-22 22:11:51 -0700

    linux proofing

--- AdminPage/DashboardPage.cpp
+++ AdminPage/DashboardPage.cpp
@@ -4,7 +4,7 @@
 
 // You may need to build the project (run Qt uic code generator) to get "ui_DashboardPage.h" resolved
 
-#include "dashboardpage.h"
+#include "DashboardPage.h"
 
 #include <QSqlError>
 #include <QSqlRecord>
